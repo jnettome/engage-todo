ebcaa5e97e86fd9460ee6097c1d18c52e5d0cb66
João Netto <hi@joaonetto.me>
2014-08-04 17:54:37 -0300

    Generating clearance views and adding default route for logged out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  constraints Clearance::Constraints::SignedIn.new do
+    # You can have the root of your site routed with "root"
+    # root 'welcome#index'
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: 'clearance/sessions#new'
+  end
 end
